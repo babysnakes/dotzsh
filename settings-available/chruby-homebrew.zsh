180f3ebf9dd572080c5b857088fd3dd18cb13b4e
Haim Ashkenazi <haim@babysnakes.org>
2014-04-23 22:53:11 +0300

    chruby changed path.

--- settings-available/chruby-homebrew.zsh
+++ settings-available/chruby-homebrew.zsh
@@ -1,5 +1,5 @@
-if [ -e /opt/homebrew/opt/chruby/share/chruby/chruby.sh ]; then
-    source /opt/homebrew/opt/chruby/share/chruby/chruby.sh
+if [ -e /usr/local/opt/chruby/share/chruby/chruby.sh ]; then
+    source /usr/local/opt/chruby/share/chruby/chruby.sh
     chruby ruby-1.9.3 >/dev/null
 fi
 alias R='ruby -v'
