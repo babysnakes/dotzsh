9815a09f3780fa90e6541bc839b08c2d3c2ba5d5
Haim Ashkenazi <haim@babysnakes.org>
2016-04-06 13:18:01 +0300

    Go 1.6 sets GO15VENDOREXPERIMENT by default.

--- settings-available/go.zsh
+++ settings-available/go.zsh
@@ -1,5 +1,5 @@
 export GOPATH=$HOME/Documents/Code/go
 add-to-path $GOPATH/bin
 
-# go 1.5.1 experimantal vendor flag
-export GO15VENDOREXPERIMENT=1
+# go 1.6 GO15VENDOREXPERIMENT is enabled by default.
+#export GO15VENDOREXPERIMENT=1
