6cdc58b8c3d4718061d808095a808b9cf3d74e82
Haim Ashkenazi <haim@babysnakes.org>
2016-10-09 14:44:12 +0300

    Manage elixir version with matching erlang.
    Using only kiex and kerl.

--- settings-available/erlang.zsh
+++ settings-available/erlang.zsh
@@ -1,5 +1,26 @@
-function use-erlang-17.0() {
-    add-to-path $HOME/.erlangs/otp-17.0/bin
-    # emacs erlang man pages
-    export ERLANG_ROOT_DIR="~/.erlangs/otp-17.0/lib/erlang"
+test -s "$HOME/.kiex/scripts/kiex" && source "$HOME/.kiex/scripts/kiex"
+
+erlang_dir="$HOME/.local/erlang/"
+elixir_13="1.3.3"
+elixir_12="1.2.4"
+
+function use-erlang-elixir() {
+    erlang_ver=$1
+    elixir_ver=$2
+    if [ -z "$erlang_ver" -o -z "$elixir_ver" ]; then
+        echo "Error seting erland and elixir version: missing erlang or elixir version!"
+    fi
+    whence kerl_deactivate >/dev/null && kerl_deactivate
+    source $erlang_dir/$erlang_ver/activate
+    kiex use $elixir_ver >/dev/null
 }
+
+function use-elixir-13() {
+    use-erlang-elixir r19 $elixir_13
+}
+
+function use-elixir-12() {
+    use-erlang-elixir r18 $lixir_12
+}
+
+use-elixir-13
