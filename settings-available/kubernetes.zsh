201b953e0b3fcae769fce31d2d2b6ae035c576cc
Haim Ashkenazi <haim@babysnakes.org>
2020-03-07 06:49:20 +0200

    kustomize completion

--- settings-available/kubernetes.zsh
+++ settings-available/kubernetes.zsh
@@ -3,4 +3,6 @@ function k8comp() {
     which minikube >&/dev/null && source <(minikube completion zsh)
     which helm >&/dev/null && source <(helm completion zsh)
     alias k=kubectl
+    autoload -U +X bashcompinit && bashcompinit
+    complete -o nospace -C /usr/local/bin/kustomize kustomize
 }
