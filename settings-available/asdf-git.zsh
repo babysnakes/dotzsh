dc68a588debc957f76f90f84ae41522b4bae5e8f
Haim Ashkenazi <haim@babysnakes.org>
2023-12-10 22:29:20 +0200

    Added asdf completions

--- settings-available/asdf-git.zsh
+++ settings-available/asdf-git.zsh
@@ -1,3 +1,8 @@
 [ -f ${HOME}/.asdf/asdf.sh ] && . ${HOME}/.asdf/asdf.sh
 
 export PATH=${HOME}/.asdf/shims:${PATH}
+
+# append completions to fpath
+fpath=(${ASDF_DIR}/completions $fpath)
+# initialise completions with ZSH's compinit
+autoload -Uz compinit && compinit
