f9e81a0461dc8470571693c2dbe5e46a39194e75
Haim Ashkenazi <haim@babysnakes.org>
2024-03-01 08:53:19 +0200

    brew completions

--- settings-available/linuxbrew.zsh
+++ settings-available/linuxbrew.zsh
@@ -1,1 +1,8 @@
 export PATH="/home/linuxbrew/.linuxbrew/bin:$PATH"
+if type brew &>/dev/null
+then
+  fpath=($(brew --prefix)/share/zsh/site-functions $fpath)
+
+  autoload -Uz compinit
+  compinit
+fi
