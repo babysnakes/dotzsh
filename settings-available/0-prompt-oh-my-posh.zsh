f5551f4fc6fbd5d0dd1b03f4041561bb7f5a1542
Haim Ashkenazi <haim@babysnakes.org>
2025-08-24 13:42:05 +0300

    Replaced oh-my-posh with Starship

--- settings-available/0-prompt-oh-my-posh.zsh
+++
@@ -1,8 +0,0 @@
-##
-## A prompt using oh my posh
-##
-
-
-# Requires manual installation of oh-my-posh:
-#    https://ohmyposh.dev/docs/linux
-eval "$(oh-my-posh --init --shell zsh --config ${DOT_ZSH}/posh-config.json)"
---
+++ settings-available/0-prompt-starship.zsh
@@ -0,0 +1,2 @@
+# Starship prompt, Needs to be installed manually.
+eval "$(starship init zsh)"
