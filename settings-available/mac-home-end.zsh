17d45fb9eaf9f6f6bcd9e28d56248203563e7c57
Haim Ashkenazi <haim@babysnakes.org>
2026-03-01 18:52:15 +0200

    Added HOME/END support

---
+++ settings-available/mac-home-end.zsh
@@ -0,0 +1,4 @@
+bindkey '^[[H'  beginning-of-line      # Home
+bindkey '^[[F'  end-of-line            # End
+bindkey '^[[1~' beginning-of-line      # Home (alternate)
+bindkey '^[[4~' end-of-line            # End (alternate)
